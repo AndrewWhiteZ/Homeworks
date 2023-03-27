c02c61a4942f1733b4248372240846fc15a17184
Андрей Фер <andrei.fer@urfu.me>
2023-03-27 21:41:20 +0500

    3/Validation

--- src/main/java/com/example/homeworks/GoodController.java
+++ src/main/java/com/example/homeworks/GoodController.java
@@ -1,6 +1,6 @@
 package com.example.homeworks;
 
-import com.example.homeworks.Dto.GoodDto;
+import com.example.homeworks.dto.GoodDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
